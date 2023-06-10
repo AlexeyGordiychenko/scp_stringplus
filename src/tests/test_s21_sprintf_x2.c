8843cf8a967f9f69073f187ebec2ca5c18408f2c
Malena Ronna <malenaro@pr-a9.nsk.21-school.ru>
2023-06-10 11:12:10 +0700

    decrease to 52 failed

--- src/tests/test_s21_sprintf_x2.c
+++ src/tests/test_s21_sprintf_x2.c
@@ -228,7 +228,7 @@ START_TEST(sprintf_16_X2) {
 END_TEST
 
 Suite *test_sprintf_x2(void) {
-  Suite *s = suite_create("\033[45m-=S21_SPRINTF_X2=-\033[0m");
+  Suite *s = suite_create("\033[33m-=s21_sprintf_x2=-\033[0m");
   TCase *tc = tcase_create("sprintf_tc");
 
   tcase_add_test(tc, sprintf_1_X2);
